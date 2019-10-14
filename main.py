5202b14c782d1ab7c6cf8b8086f335e0efbe58c2
peach-weiyl <nbzdds08@163.com>
2019-10-14 10:27:49 +0800

    初步架构

---
+++ main.py
@@ -0,0 +1,13 @@
+# Author:尉玉林(Mr.Wei)
+
+# Create Date:2019/10/14
+
+# Edition:V1.0.0
+
+# Python自带库
+import sys
+# 第三方库
+from PySide2.QtWidgets import QMainWindow
+# 自己的包
+import code.HslCommunication
+from UI2PY.MainWindow import Ui_MainWindow
